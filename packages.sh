5c75007ebe99130c4226131f75857f49ed4c8c84
Bootloopy <100815431+Bootloopy@users.noreply.github.com>
2022-03-02 21:23:13 -0500

    Updated Repo

--- packages.sh
+++ packages.sh
@@ -4,7 +4,3 @@ dpkg-deb -bZgzip projects/OGAppleLogo-i6-9 debs
 dpkg-deb -bZgzip projects/GoogleSearchXI debs
 dpkg-deb -bZgzip projects/S9InspiredLS debs
 dpkg-deb -bZgzip projects/S9InspiredSB debs
-<<<<<<< HEAD
-dpkg-deb -bZgzip projects/S10Inspired debs
-=======
->>>>>>> 1c4b6985eb8398958d13f970a463477946278f5f
---
+++ projects/GoogleSearchXIV/DEBIAN/control.sh
@@ -0,0 +1,9 @@
+Package: com.booloopy.googlesearchxiv
+Version: 1.0.0 b1.0
+Name: GoogleSearchXIV
+Section: Widgets
+Depends:
+Description: Requires XenInfo to work. A Google search widget for iOS 14.0-14.x.
+Architecture: iphoneos-arm
+Author: Bootloopy (IDAoADI) <>
+Maintainer: Bootloopy (IDAoADI) <>
