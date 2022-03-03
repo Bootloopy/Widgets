d4f32b400c723b2316d8633fce7b19be3ca4eb40
Bootloopy <100815431+Bootloopy@users.noreply.github.com>
2022-03-02 20:59:17 -0500

    Updated Repo

---
+++ packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dpkg-deb -bZgzip projects/SpoofCard debs
+dpkg-deb -bZgzip projects/OGAppleLogo-i6-9 debs
+dpkg-deb -bZgzip projects/GoogleSearchXI debs
+dpkg-deb -bZgzip projects/S9InspiredLS debs
+dpkg-deb -bZgzip projects/S9InspiredSB debs
+<<<<<<< HEAD
+dpkg-deb -bZgzip projects/S10Inspired debs
+=======
+>>>>>>> 1c4b6985eb8398958d13f970a463477946278f5f
