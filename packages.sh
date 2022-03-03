02d1ee2cf86e61f862c6e8964ef7f1368408dc71
Bootloopy <100815431+Bootloopy@users.noreply.github.com>
2022-03-02 21:31:40 -0500

    Added Test Package

--- packages.sh
+++ packages.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/SpoofCard debs
-dpkg-deb -bZgzip projects/OGAppleLogo-i6-9 debs
-dpkg-deb -bZgzip projects/GoogleSearchXI debs
-dpkg-deb -bZgzip projects/S9InspiredLS debs
-dpkg-deb -bZgzip projects/S9InspiredSB debs
+dpkg-deb -bZgzip projects/GoogleSearchXIV debs
