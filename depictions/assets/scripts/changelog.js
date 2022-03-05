d768d66a00e243760e8638c274ea8c519606993c
Bootloopy <100815431+Bootloopy@users.noreply.github.com>
2022-03-05 18:57:31 -0500

    Updated Depictions

--- depictions/assets/scripts/changelog.js
+++ depictions/assets/scripts/changelog.js
@@ -1,13 +1,29 @@
 var more = document.getElementById("more");
 var log = document.getElementById("log");
+var status = 1;
 
-window.onload = function() {
-	log.style.display = "none";
-
-	function loadMore() {
-		log.style.display = "block";
-		more.style.display = "none";
-    }
-
-    more.addEventListener("click", loadMore);
+function toggle() {
+	if (status == 1) {
+		showMore();
+	} else {
+		showLess();
+	}
 }
+
+function showMore() {
+	setTimeout(function() {
+		status = 0;
+	}, 100);
+	log.style.display = "block";
+	more.innerHTML = "Show Less"
+}
+
+function showLess() {
+	setTimeout(function() {
+		status = 1;
+	}, 100);
+	log.style.display = "none";
+	more.innerHTML = "More Versions";
+}
+
+more.addEventListener("click", toggle);
