d9b9e4697d0314ca250fa413b1cfe5a099e172b1
Bootloopy <100815431+Bootloopy@users.noreply.github.com>
2022-03-06 21:41:34 -0500

    Update changelog.js

--- depictions/assets/scripts/changelog.js
+++ depictions/assets/scripts/changelog.js
@@ -1,7 +1,7 @@
 var more = document.getElementById("more");
 
 function moreVersions() {
-	window.open("https://bootloopy.github.io/depictions/projects/googlesearchxiv/changelog.html");
+	window.location = "https://bootloopy.github.io/depictions/projects/googlesearchxiv/changelog.html";
 }
 
 more.addEventListener("click", moreVersions);
